740b469f7c01fa2cec3ff36962837d24f3f3ebb2
EzeSandes <77078541+EzeSandes@users.noreply.github.com>
2022-04-13 11:57:05 -0300

    Update ctype.c

--- StringLibrary/ctype.c
+++ StringLibrary/ctype.c
@@ -1,5 +1,9 @@
 #define IS_CAPITALLETTER(x)    ((x) >= 'A' && (x) <= 'Z')
 #define IS_LOWERCASE(x)    ((x) >= 'a' && (x) <= 'z')
+
+#define A_MINUSC(x)    ((x) >= 'a' && (x) <= 'z' ? (x) : ((x) + ('a' - 'A')))
+#define A_MAYUSC(x)    ((x) >= 'A' && (x) <= 'Z' ? (x) : ((x) - ('a' - 'A')))
+
 //////////////////////////////
 
 int myTolower(int ch)
@@ -14,4 +18,16 @@ int myToupper(int ch)
     return IS_LOWERCASE(ch)?   ch - 32 : ch;
 }
 
-///////
+/////////////////////////////////// Another options
+
+int to_lower(int ch)
+{
+    return A_MINUSC(ch);
+}
+
+/////////
+
+int to_upper(int ch)
+{
+    return A_MAYUSC(ch);
+}
